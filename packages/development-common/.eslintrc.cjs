59c25193ce7c6c709987e7f38c753ae13b939e96
Riley Cullen <cullenriley@seattleu.edu>
2023-06-24 16:03:23 -0700

    Typescript and eslint added

---
+++ packages/development-common/.eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:t@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true
+};
