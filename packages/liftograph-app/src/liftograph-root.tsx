d6838ef00c0c6295be9befd83b3838dc3dc6375b
Riley Cullen <cullenriley@seattleu.edu>
2023-07-16 14:29:38 -0700

    Toolbar set up

--- packages/liftograph-app/src/liftograph-root.tsx
+++ packages/liftograph-app/src/liftograph-root.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import WorkoutEditor from '@liftograph/workout-editor-ui';
-import styles from './liftograph-root.less';
+import { IconButton, Toolbar as BaseToolbar, Typography, AppBar, CssBaseline, Box } from '@mui/material';
+import { Home } from '@mui/icons-material';
 
 export const DATA_TESTID = 'liftograph-application-root';
 
+/** Entry point to the Liftograph application */
 export function LiftographRoot() {
     return (
-        <h1
-            data-testid={DATA_TESTID}
-            className={styles.liftographRoot}
-        >
-            I'm a react component!
-           <WorkoutEditor />
-        </h1>
+        <Box data-testid={DATA_TESTID} sx={{border: '4px solid red'}}>
+            <CssBaseline />
+            <AppBar position='sticky'>
+                <Toolbar />
+            </AppBar>
+            <p>hi</p>
+        </Box>
     );
 }
 
+function Toolbar() {
+    return (
+        <BaseToolbar sx={
+            {
+                border: '1px solid blue'
+            }
+        }>
+            <IconButton 
+                aria-label='home'
+                sx={{
+                    marginLeft: 0,
+                    marginRight: 5
+                }}
+            >
+                <Home />
+            </IconButton>
+            <Typography variant='h4'>Liftograph</Typography>
+        </BaseToolbar>
+    );
+}
