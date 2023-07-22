768dc8515ec66133ed891271471148e89e8b9bd4
Riley Cullen <cullenriley@seattleu.edu>
2023-07-22 16:01:44 -0700

    Tests 2

--- packages/liftograph-app/test/unit/components/tab-panel.spec.tsx
+++ packages/liftograph-app/test/unit/components/tab-panel.spec.tsx
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 const TAB_INDEX = 'mock-tab-index';
+const MOCK_CONTENT_DATA_TESTID = 'mock-test-id';
 
 describe(TabPanel.name, () => {
     it('should render content if currentTab equals tabIndex', () => {
@@ -11,6 +12,8 @@ describe(TabPanel.name, () => {
         const tabContainer= screen
             .getByTestId(`${BASE_DATA_TESTID}_${TAB_INDEX}`);
         expect(tabContainer).toBeVisible();
+        expect(screen.getByTestId(MOCK_CONTENT_DATA_TESTID))
+            .toBeInTheDocument();
     });
     
     it('should not render content if currentTab does not equal tabIndex', () => {
@@ -18,12 +21,16 @@ describe(TabPanel.name, () => {
         const tabContainer= screen
             .getByTestId(`${BASE_DATA_TESTID}_${TAB_INDEX}`);
         expect(tabContainer).not.toBeVisible();
+        expect(screen.queryByTestId(MOCK_CONTENT_DATA_TESTID))
+            .toBeNull();
     });
     
     function renderTabPanel(currentTab: string, tabIndex: string) {
         render(
             <TabPanel tabIndex={tabIndex} currentTab={currentTab}>
-                Mock Content
+                <div data-testid={MOCK_CONTENT_DATA_TESTID}>
+                    Mock tab content
+                </div>
             </TabPanel>
         );
     }
