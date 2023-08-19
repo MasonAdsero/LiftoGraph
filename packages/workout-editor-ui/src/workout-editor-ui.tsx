8bec95fa6125266e890d08a46f0a7aee1ffaaf15
MasonAdsero <madsero1@gmail.com>
2023-08-19 16:37:22 -0700

    Various code reorganizations, button errors, and button functionality

--- packages/workout-editor-ui/src/workout-editor-ui.tsx
+++ packages/workout-editor-ui/src/workout-editor-ui.tsx
@@ -20,7 +20,11 @@ const testExerciseTwo: Exercise = {
 
 test.push(testExercise, testExerciseTwo);
 
-function DeleteDialog(){
+interface DeleteDialogProps{
+    index:number;
+}
+
+function DeleteDialog(props: DeleteDialogProps){
     const [open, setOpen] = useState(false);
 
     const handleClickOpen = () => {
@@ -34,7 +38,7 @@ function DeleteDialog(){
     const handleCloseDelete = () => {
         //TODO: Change to remove in accordance with backend rather than test list
         //The next line needs the index
-        test.splice(0,1);
+        test.splice(props.index,1);
         setOpen(false);
     }
 
@@ -67,12 +71,51 @@ function AddExercise(){
     const [exerciseForm, setForm] = useState('');
     const [exerciseSets, setSet] = useState(0);
     const [exerciseReps, setRep] = useState(0);
+    const [exerciseErrors, setErrors] = useState<Record<string,string>>({
+        exerciseName: "",
+        exerciseForm: "",
+        exerciseSets: "",
+        exerciseReps: ""
+    });
 
-    const AddToList = () => {
+    const AddToList = (event: React.FormEvent) => {
         //TODO: Change to add exercise to the apps list rather than test list
-        test.push({name: exerciseName, form: exerciseForm, sets: exerciseSets, repetitions: exerciseReps});
-        setAdd(!addExercise);
+        event.preventDefault();
+        if(validateForm()){
+            test.push({name: exerciseName, form: exerciseForm, sets: exerciseSets, repetitions: exerciseReps});
+            setAdd(!addExercise);
+            clearForm();
+        }
     };
+
+    const validateForm = () => {
+        let isValid = true;
+        setErrors({});
+        const errors: Record<string, string> = {};
+        if(exerciseName == '')
+        {
+            errors["exerciseName"] = 'Exercise must have a name';
+            isValid = false;
+        }
+        if(exerciseSets < 1) {
+            errors["exerciseSets"] = 'Sets must be more than one';
+            isValid = false;
+        }
+        if(exerciseReps < 1) {
+            errors["exerciseReps"] = 'Reps must be more than one';
+            isValid = false;
+        }
+        setErrors(errors);
+        return isValid;
+    }
+
+    const clearForm = () => {
+        setName('');
+        setForm('');
+        setSet(0);
+        setRep(0);
+        setAdd(!addExercise);
+    }
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column'}}>
@@ -80,14 +123,54 @@ function AddExercise(){
                 Add
             </Button> : (
             <>
-                <Button sx={{marginLeft: 'auto'}} variant='outlined' startIcon={<CloseIcon/>} onClick={()=> setAdd(!addExercise)}>Cancel</Button>
-                <Box component='form' sx={{flex: 1, display: 'flex', flexDirection: 'column',width: '100%', maxWidth: 360, bgcolor: 'background.paper', padding: '15px', paddingTop: '10px'}}>
-                    <TextField fullWidth id='exercise-name' label='name' variant='standard' value={exerciseName} onChange={(e) => setName(e.target.value)}/>
-                    <TextField fullWidth id='exercise-form' label='form' variant='standard' value={exerciseForm} onChange={(e) => setForm(e.target.value)}/>
-                    <TextField fullWidth id='exercise-sets' inputProps={{min:0}} label='sets' type='number'  variant='standard' value={exerciseSets} onChange={(e) => setSet(parseInt(e.target.value))}/>
-                    <TextField fullWidth id='exercise-reps' inputProps={{min:0}} label='repetitions' type='number' variant='standard' value={exerciseReps} onChange={(e) => setRep(parseInt(e.target.value))}/>
-                </Box>
-                <Button sx={{marginLeft: 'auto'}} variant='outlined' onClick={() => AddToList}>Confirm</Button>
+                <Button sx={{marginLeft: 'auto'}} variant='outlined' startIcon={<CloseIcon/>} onClick={clearForm}>Cancel</Button>
+                <form onSubmit={AddToList}>
+                    <Box component='form' sx={{flex: 1, display: 'flex', flexDirection: 'column',width: '100%', maxWidth: 360, bgcolor: 'background.paper', padding: '15px', paddingTop: '10px', margin: '0 auto'}}>
+                        <TextField
+                            fullWidth
+                            id='exercise-name'
+                            label='name'
+                            variant='standard'
+                            value={exerciseName}
+                            onChange={(e) => setName(e.target.value)}
+                            error={!!exerciseErrors.exerciseName}
+                            helperText={exerciseErrors.exerciseName}
+                        />
+                        <TextField fullWidth
+                            id='exercise-form'
+                            label='form'
+                            variant='standard'
+                            value={exerciseForm}
+                            onChange={(e) => setForm(e.target.value)}
+                        />
+                        <TextField
+                            fullWidth id='exercise-sets'
+                            inputProps={{min:1}}
+                            label='sets'
+                            type='number'
+                            variant='standard'
+                            value={exerciseSets}
+                            onChange={(e) => setSet(parseInt(e.target.value))}
+                            error={!!exerciseErrors.exerciseSets}
+                            helperText={exerciseErrors.exerciseSets}
+                        />
+                        <TextField
+                            fullWidth
+                            id='exercise-reps'
+                            inputProps={{min:1}}
+                            label='repetitions'
+                            type='number'
+                            variant='standard'
+                            value={exerciseReps}
+                            onChange={(e) => setRep(parseInt(e.target.value))}
+                            error={!!exerciseErrors.exerciseReps}
+                            helperText={exerciseErrors.exerciseReps}
+                        />
+                    </Box>
+                    <div style={{ display: 'flex', flexDirection: 'column'}}>
+                        <Button type='submit' sx={{marginTop: '10px', marginLeft: 'auto'}} variant='outlined'>Confirm</Button>
+                    </div>
+                </form>
             </>
             )
             }
@@ -103,7 +186,7 @@ export function ExerciseList(){
                     <List>
                         {test.map((item, index) => (
                         <ListItem key={index} secondaryAction={
-                                    <DeleteDialog/>
+                                    <DeleteDialog index={index}/>
                                 }>
                             <ListItemButton>
                                 <ListItemText primary={item.name}/>
